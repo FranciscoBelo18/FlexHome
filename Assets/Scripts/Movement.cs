621dd9474d622f966d252881738e6a935c132741
FranciscoBelo18 <96338080+FranciscoBelo18@users.noreply.github.com>
2025-03-06 15:47:53 +0000

    adiçao de andar por AI no npc

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private NavMeshAgent agent = null;
+    private GameObject[] ObjectToGo;
+    private int currentObject;
+
+
+    void Start()
     {
-        if (collision.gameObject.tag == "treadmill")
-        {
-            Debug.Log("passadeira");
-        }
+        agent = this.GetComponent<NavMeshAgent>();
+        ObjectToGo = GameObject.FindGameObjectsWithTag("Treadmill");
+        Debug.Log("Numero de passadeiras no gym: " + ObjectToGo.Length.ToString());
     }
 
+    void Update()
+    {
+        if(agent.hasPath == false)
+        {
+            currentObject = Random.Range(0, ObjectToGo.Length);
+            agent.SetDestination(ObjectToGo[currentObject].transform.position);
+
+        }
+    }
 }
