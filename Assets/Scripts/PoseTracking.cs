921aeab5fbef775e48ccbb992b0c2fa2a1e56e64
FranciscoBelo18 <96338080+FranciscoBelo18@users.noreply.github.com>
2025-04-08 15:45:05 +0100

    feito para a mao e começo da adaptaçao para a pose corpo inteiro

---
+++ Assets/Scripts/PoseTracking.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoseTracking : MonoBehaviour
+{
+    public UDPReceive udpReceive;
+    public GameObject[] PosePoints;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = udpReceive.data;
+
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length - 1, 1);
+
+        string[] points = data.Split(',');
+
+        for (int i = 0; i < PosePoints.Length; i++)
+        {
+            /*float x = float.Parse(points[i * 3])/80-8;
+            float y = float.Parse(points[i * 3 + 1])/80-5;
+            float z = float.Parse(points[i * 3 + 2])/80;*/
+
+            float x = 7 - float.Parse(points[i * 3]) / 100;
+            float y = float.Parse(points[i * 3 + 1]) /100;
+            float z = float.Parse(points[i * 3 + 2]) / 100;
+
+            PosePoints[i].transform.localPosition = new Vector3(x, y, z);
+        }
+
+    }
+}
